0507c014d1870cac6375aaf18f2a17da82b43410
josdev <chaqquererj@gmail.com>
2025-11-12 11:45:35 -0500

    haciendo funcional las rutas en el navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppNavbarComponent } from './shared/app-navbar/app-navbar.component';
+import { NavbarComponent } from './shared/app-navbar/app-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppNavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/shared/app-navbar/app-navbar.component.ts
+++ src/app/shared/app-navbar/app-navbar.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './app-navbar.component.html',
 })
-export class AppNavbarComponent {}
+export class NavbarComponent {
+  router = inject(Router);
+  routes = routes
+    .map((route) => ({
+      path: route.path,
+      title: `${route.title ?? 'Maps en Angular'}`,
+    }))
+    .filter((route) => route.path != '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map((event) => event.url),
+    map((url) => routes.find((route) => `/${route.path}` == url)?.title)
+  );
+}
