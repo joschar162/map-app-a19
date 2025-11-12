745d89ea305be765ce9aee929084bcc059f7ac67
josdev <chaqquererj@gmail.com>
2025-11-12 09:21:12 -0500

    file system ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbarComponent } from './shared/app-navbar/app-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'maps-app';
---
+++ src/app/shared/app-navbar/app-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './app-navbar.component.html',
+})
+export class AppNavbarComponent {}
