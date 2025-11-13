61b6fc5a8acd6ebe969103de1263d31026b8d22b
josdev <chaqquererj@gmail.com>
2025-11-12 20:43:28 -0500

    controlando zom del mapa

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,36 +1,56 @@
-import { Component, viewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  viewChild,
+  ElementRef,
+  AfterViewInit,
+  signal,
+  effect,
+} from '@angular/core';
 
 import { environment } from '../../../environments/environment';
 import { Map } from 'maplibre-gl';
+import { DecimalPipe } from '@angular/common';
 
 const myMapTilerKey = environment.maptilerKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './fullscreen-map-page.component.html',
-  styles: `
-div{
-  width:100vw;
-  height:calc(100vh - 64px);
-
-}
-`,
+  styleUrl: './fullscreen-map-page.component.css',
 })
 export class FullscreenMapPageComponent implements AfterViewInit {
   divElement = viewChild<ElementRef>('map');
+  map = signal<Map | null>(null);
+  myZoom = signal(14);
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+    this.map()?.setZoom(this.myZoom());
+    // this.map()?.zoomTo(this.myZoom());
+  });
 
   async ngAfterViewInit() {
     if (!this.divElement()?.nativeElement) return;
     await new Promise((resolve) => setTimeout(resolve, 80));
     const element = this.divElement()!.nativeElement;
 
-    const initialState = { lng: -77.0428, lat: -12.0464, zoom: 9 };
+    const initialState = { lng: -77.0428, lat: -12.0464, zoom: this.myZoom() };
     const map = new Map({
       container: element,
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${myMapTilerKey}`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
     });
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.myZoom.set(newZoom);
+    });
+
+    this.map.set(map);
   }
 }
