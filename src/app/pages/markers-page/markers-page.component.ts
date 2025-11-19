3dc2215725c7fa418bb2d3d03a2635a1d851a065
josdev <chaqquererj@gmail.com>
2025-11-18 21:02:53 -0500

    generando marcadores dinamicos

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -8,6 +8,7 @@ import {
 
 import {
   FullscreenControl,
+  LngLatLike,
   Map,
   Marker,
   NavigationControl,
@@ -15,8 +16,15 @@ import {
 } from 'maplibre-gl';
 
 import { environment } from '../../../environments/environment';
+import { MapMouseEvent } from 'maplibre-gl';
+import { v4 as uuIdV4 } from 'uuid';
 
 const myMapTilerKey = environment.maptilerKey;
+
+interface MyMarker {
+  id: string;
+  mapTilerMarker: Marker;
+}
 
 @Component({
   selector: 'app-markers-page',
@@ -26,6 +34,8 @@ const myMapTilerKey = environment.maptilerKey;
 export class MarkersPageComponent implements AfterViewInit {
   divElement = viewChild<ElementRef>('marker');
   map = signal<Map | null>(null);
+  markers = signal<MyMarker[]>([]);
+
   async ngAfterViewInit() {
     if (!this.divElement()?.nativeElement) return;
     await new Promise((resolve) => setTimeout(resolve, 80));
@@ -38,19 +48,58 @@ export class MarkersPageComponent implements AfterViewInit {
       zoom: 16,
     });
 
-    const marker = new Marker({
-      draggable: true,
-    })
-      .setLngLat([-72.9445, -14.3896])
-      .addTo(map);
+    // const marker = new Marker({
+    //   draggable: true,
+    // })
+    //   .setLngLat([-72.9445, -14.3896])
+    //   .addTo(map);
 
-    marker.on('dragend', (event) => {
-      console.log(event);
-    });
+    // marker.on('dragend', (event) => {
+    //   console.log(event);
+    // });
 
     this.mapListeners(map);
   }
+
   mapListeners(map: Map) {
-    console.log('object');
+    map.on('click', (event) => this.mapClick(event));
+
+    this.map.set(map);
+  }
+
+  mapClick(event: MapMouseEvent) {
+    if (!this.map()) return;
+
+    const map = this.map()!;
+
+    const coords = event.lngLat;
+
+    this.setMarker(map, coords);
+
+    // console.log(event.lngLat.lng);
+  }
+
+  setMarker(map: Map, coords: LngLatLike) {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const mapTilerMarker = new Marker({
+      draggable: true,
+      color: color,
+    })
+      // .setLngLat([lng, lat])
+      .setLngLat(coords)
+      .addTo(map);
+
+    const myNewMarker: MyMarker = {
+      id: uuIdV4(),
+      mapTilerMarker: mapTilerMarker,
+    };
+
+    // this.markers.set([myNewMarker, ...this.markers()]);
+    this.markers.update((markers) => [myNewMarker, ...markers]);
+
+    console.log(this.markers());
   }
 }
