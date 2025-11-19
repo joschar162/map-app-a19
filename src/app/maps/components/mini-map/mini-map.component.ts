1142631e6239f8b78ee8cff7b0b63cb7709d6ac8
josdev <chaqquererj@gmail.com>
2025-11-19 13:28:49 -0500

    mostrando varias mapas en pantalla

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, input, signal, viewChild } from '@angular/core';
+import { Map, Marker } from 'maplibre-gl';
+import { environment } from '../../../../environments/environment';
+
+const myMapTilerKey = environment.maptilerKey;
 
 @Component({
   selector: 'app-mini-map',
   imports: [],
   templateUrl: './mini-map.component.html',
+  styles: `
+
+
+  `,
 })
-export class MiniMapComponent {}
+export class MiniMapComponent {
+  divElement = viewChild<ElementRef>('mapRef');
+  map = signal<Map | null>(null);
+  myZoom = signal(14);
+
+  lngLat = input.required<{ lng: number; lat: number }>();
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+    await new Promise((resolve) => setTimeout(resolve, 80));
+    const element = this.divElement()!.nativeElement;
+
+    const { lng, lat } = this.lngLat();
+    const map = new Map({
+      container: element,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${myMapTilerKey}`,
+      center: [lng, lat],
+      zoom: this.myZoom(),
+      interactive: false,
+      pitch: 50,
+    });
+    new Marker().setLngLat([lng, lat]).addTo(map);
+  }
+
+  ngOnDestroy(): void {
+    this.map()?.remove();
+  }
+}
